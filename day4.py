0bd8c0499c3d4cfc614286128340c214f1605799
Matthew <merobinaugh903@gmail.com>
2022-12-04 14:27:26 -0700

    Made range determination into function

--- day4.py
+++ day4.py
@@ -1,14 +1,18 @@
+def get_ranges(pair):
+    pair = pair.rstrip('\n')
+    set1start = int(pair[0:pair.index('-')])
+    set1end = int(pair[pair.index('-')+1:pair.index(',')]) + 1
+    set2start = int(pair[pair.index(',')+1:pair.rindex('-')]) 
+    set2end = int(pair[pair.rindex('-')+1:]) + 1
+    return [set(range(set1start,set1end)), set(range(set2start,set2end))]
+
 def part1(inp_file):
     cont_pairs = 0
     with open(inp_file) as inp:
         for pair in inp:
-            pair = pair.rstrip('\n')
-            set1start = int(pair[0:pair.index('-')])
-            set1end = int(pair[pair.index('-')+1:pair.index(',')]) + 1
-            set2start = int(pair[pair.index(',')+1:pair.rindex('-')]) 
-            set2end = int(pair[pair.rindex('-')+1:]) + 1
-            set1 = set(range(set1start,set1end))
-            set2 = set(range(set2start,set2end))
+            sets = get_ranges(pair)
+            set1 = sets[0]
+            set2 = sets[1]
             intersect = set(set1 & set2)
             if len(intersect) == len(set1) or len(intersect) == len(set2):
                 cont_pairs+=1
@@ -18,13 +22,9 @@ def part2(inp_file):
     cont_pairs = 0
     with open(inp_file) as inp:
         for pair in inp:
-            pair = pair.rstrip('\n')
-            set1start = int(pair[0:pair.index('-')])
-            set1end = int(pair[pair.index('-')+1:pair.index(',')]) + 1
-            set2start = int(pair[pair.index(',')+1:pair.rindex('-')]) 
-            set2end = int(pair[pair.rindex('-')+1:]) + 1
-            set1 = set(range(set1start,set1end))
-            set2 = set(range(set2start,set2end))
+            sets = get_ranges(pair)
+            set1 = sets[0]
+            set2 = sets[1]
             intersect = set(set1 & set2)
             if len(intersect) > 0:
                 cont_pairs+=1
